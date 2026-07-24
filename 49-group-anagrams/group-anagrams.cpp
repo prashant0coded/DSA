279e636f14f741642052555bdb39090fb669946f
prashant0coded <prashantrajput01234@gmail.com>
2026-07-24 22:26:45 +0530

    Time: 12 ms (83.68%) | Memory: 26.1 MB (49.30%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        if(strs.empty()){
+            return ans;
+        }
+        unordered_map<string,vector<string>>mpp;
+        for(string str:strs){
+            //storing the str in new variable (string key) and sort it
+            string key=str;
+            sort(key.begin(),key.end());
+            mpp[key].push_back(str);
+        }
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+    }
+};
