73158a0901ab190857280448ffb6a491f242237f
prashant0coded <prashantrajput01234@gmail.com>
2026-07-24 19:16:08 +0530

    Time: 146 ms (5.02%) | Memory: 116.2 MB (7.27%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return true;
-            }
-        }
-        return false;
+       set<int>st;
+       for(int x :nums){
+        st.insert(x);
+       }
+
+       if(st.size()!=nums.size()){
+        return true;
+       }
+       else return false;
     }
 };
+//TC=nLOGn+N
